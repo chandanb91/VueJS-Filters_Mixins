dc7c12cc75bbae1a4aea036334d0d507e70897df
Chandan <chandan7791@gmail.com>
2018-09-26 15:42:47 +0530

    Mixins and Scope

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
                 <h1>Filters & Mixins</h1>
                 <p>{{ text | toUppercase | to-lowercase }}</p>
                 <hr>
+                <button @click="fruits.push('Berries')">Add an Item</button>
                 <input type="text" v-model="filterText">
                 <ul>
                     <li v-for="(fruit,index) in filteredFruits" :key="index">{{ fruit }}</li>
